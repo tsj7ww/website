2778350dc5f78b9518c12a0d921aae69c39e4a9e
Trevor Jordan <20461601+tsj7ww@users.noreply.github.com>
2023-04-29 12:55:13 -0400

    revert to wordpress for about me page

--- commands.sh
+++ commands.sh
@@ -1,2 +1,4 @@
-cp -r ~/Documents/github/website/index.html ~/Documents/github/website/assets ~/Documents/github/tsj7ww.github.io/
+# blog
 cp -r ~/Documents/github/website/blog/public/* ~/Documents/github/tsj7ww.github.io/blog/
+# home page
+# cp -r ~/Documents/github/website/about/index.html ~/Documents/github/website/about/assets ~/Documents/github/tsj7ww.github.io/
