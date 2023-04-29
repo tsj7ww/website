b2d5d468dd5df799b3385f939773be74994312fa
Trevor Jordan <20461601+tsj7ww@users.noreply.github.com>
2023-04-29 13:35:37 -0400

    cleanup blog

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
 # blog
-cp -r ~/Documents/github/website/blog/public/* ~/Documents/github/tsj7ww.github.io/blog/
+cp -r ~/Documents/github/website/blog/public/* ~/Documents/github/tsj7ww.github.io/
 # home page
 # cp -r ~/Documents/github/website/about/index.html ~/Documents/github/website/about/assets ~/Documents/github/tsj7ww.github.io/
